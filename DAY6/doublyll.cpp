07b6cb617f8b1446026c8546958604f239849f8c
Parkhi Agarwal <agparkhi96@gmail.com>
2025-08-06 22:43:03 +0530

    .

--- DAY6/doublyll.cpp
+++ DAY6/doublyll.cpp
@@ -19,3 +19,32 @@ class Node {
         back=nullptr;
     }
 };
+
+// converting array into a doubly linked list
+Node* arrtoDLL(vector<int> &arr) {
+    Node* head= new Node(arr[0]);
+    Node* prev= head;
+    for (int i=1; i<=arr.size()-1; i++) {
+        Node* temp = new Node (arr[i], NULL, prev);
+        prev->next=temp;
+        prev=temp;
+        
+    }
+    return head;
+}
+
+// printing a linked list
+void print(Node* head) {
+    while (head!=NULL) {
+        cout << head->data << " ";
+        head=head->next;
+    }
+}
+
+int main() {
+    vector<int> arr= {1,3,2,4};
+    Node* head = arrtoDLL(arr);
+    print(head);
+
+    return 0;
+}
